066b6a4504788a11637850de153396760b1df068
alvinlukmanNH <galinx20gmail.com>
2018-10-01 22:27:09 +0700

    PerbandinganDuabuahNilai

---
+++ src/PerbandinganDuabuahNilai.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama      : Alvin Lukman Nulhakim 
+ * Kelas     : IF3 
+ * NIM       : 10117095 
+ * Deskripsi : program ini menampilkam perbandingan nilai dari besar ke kecil 
+               atau kecil ke besar
+ */
+public class PerbandinganDuabuahNilai {
+
+   
+    public static void main(String[] args) {
+       //        deklarasi variabel
+        int n1;
+        int n2;
+        String lagi; //variabel pengontrol aktifitas
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.println("========Program Perbandingan Nilai========");
+//        menggunakan looping do while
+        do {            
+            System.out.print("Masukkan nilai pertama : ");
+            n1 = scn.nextInt();
+            System.out.print("Masukkan nilai kedua : ");
+            n2 = scn.nextInt();
+            
+//            rumus perbandingan
+            if (n1 > n2) {
+                System.out.println("Hasil : "+n1+" Lebih besar dari "+n2+"\n");
+            }else if(n1 < n2){
+                System.out.println("Hasil : "+n1+" Lebih kecil dari "+n2+"\n");
+            }else
+                System.out.println("Hasil : "+n1+" sama dengan "+n2+"\n");
+                
+//            ketikkan "Tidak" bila ingin berhenti aktifitas
+            System.out.print("Ulangi Aktifitas ? (Ya/Tidak) : ");
+            lagi = scn.next();
+            
+            System.out.println("");//memberi jarak satu baris
+        } while (!lagi.equals("Tidak"));
+    }
+    
+}
